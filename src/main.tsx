4fadd589634095cf374836761bc88c5acbafd301
HikenCoding <erkandeg94@gmail.com>
2024-09-29 22:58:58 +0200

    Dashboard now working

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App.tsx'
 import './index.css'
-import Navbar from './Navbar.tsx';
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <Navbar />
+    <App /> 
   </StrictMode>,
 )
